e67414b2928046fe431d21c4c65e58caad7fcdda
Paul Bond <paulandrewbond@outlook.com>
2020-02-19 14:44:57 +0000

    first commit

---
+++ navigation/useLinking.js
@@ -0,0 +1,11 @@
+import { useLinking } from "@react-navigation/native";
+
+const useLinking = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default useLinking;
