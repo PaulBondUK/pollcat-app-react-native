99fdbe958eddf2905140a5d54a5e8a25e22a1db4
Paul Bond <paulandrewbond@outlook.com>
2020-02-21 09:26:06 +0000

    convert to class

--- Login/LoginOrCreate.js
+++ Login/LoginOrCreate.js
@@ -1,23 +1,28 @@
 import * as React from "react";
 import { Button, View, Text } from "react-native";
+import firebase from "../Auth/Firebase";
 
-export default function LoginOrCreate({ navigation }) {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-      }}
-    >
-      <Button
-        title="Login"
-        onPress={() => navigation.navigate("LoginHandler")}
-      />
-      <Button
-        title="Create Account"
-        onPress={() => navigation.navigate("CreateAccountHandler")}
-      />
-    </View>
-  );
+const user = firebase.auth().currentUser;
+
+export default class LoginOrCreate extends React.Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <Button
+          title="Login"
+          onPress={() => this.props.navigation.navigate("LoginHandler")}
+        />
+        <Button
+          title="Sign Up"
+          onPress={() => this.props.navigation.navigate("CreateAccountHandler")}
+        />
+      </View>
+    );
+  }
 }
