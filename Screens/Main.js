fcdf97302057f34d40938144531ff76a531f3575
Paul Bond <paulandrewbond@outlook.com>
2020-02-21 21:44:03 +0000

    add tab buttons

--- Screens/Main.js
+++ Screens/Main.js
@@ -15,10 +15,15 @@ import {
   Button
 } from "native-base";
 import firebase from "../Auth/Firebase";
+import Account from "./Account/Account";
+import CityProfile from "./CityProfile/CityProfile";
+import History from "./History/History";
+import TodaysPoll from "./TodaysPoll/TodaysPoll";
 
 export default class Main extends Component {
   state = {
-    user: null
+    user: null,
+    activePage: "TodaysPoll"
   };
 
   render() {
@@ -30,23 +35,48 @@ export default class Main extends Component {
       <Container>
         <Header />
         <Content>
-          <Footer>
-            <FooterTab>
-              <Button vertical>
-                <Icon name="ios-today" />
-                <Text>Today's Poll</Text>
-              </Button>
-              <Button vertical>
-                <Icon name="" />
-                <Text></Text>
-              </Button>
-              <Button vertical>
-                <Icon name="" />
-                <Text></Text>
-              </Button>
-            </FooterTab>
-          </Footer>
+          <Account />
         </Content>
+        <Footer>
+          <FooterTab>
+            <Button
+              style={styles.button}
+              vertical
+              onPress={() => this.setState({ activePage: "TodaysPoll" })}
+              active={this.state.activePage === "TodaysPoll"}
+            >
+              <Icon type="MaterialCommunityIcons" name="calendar-today" />
+              <Text style={styles.buttonText}>Today's Poll</Text>
+            </Button>
+            <Button
+              style={styles.button}
+              vertical
+              onPress={() => this.setState({ activePage: "History" })}
+              active={this.state.activePage === "History"}
+            >
+              <Icon type="MaterialCommunityIcons" name="history" />
+              <Text style={styles.buttonText}>History</Text>
+            </Button>
+            <Button
+              style={styles.button}
+              vertical
+              onPress={() => this.setState({ activePage: "CityProfile" })}
+              active={this.state.activePage === "CityProfile"}
+            >
+              <Icon type="MaterialCommunityIcons" name="city" />
+              <Text style={styles.buttonText}>City Profile</Text>
+            </Button>
+            <Button
+              style={styles.button}
+              vertical
+              onPress={() => this.setState({ activePage: "Account" })}
+              active={this.state.activePage === "Account"}
+            >
+              <Icon type="MaterialCommunityIcons" name="account" />
+              <Text style={styles.buttonText}>Account</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
       </Container>
     );
   }
@@ -75,3 +105,28 @@ export default class Main extends Component {
     });
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    justifyContent: "center"
+  },
+  input: {
+    marginTop: 10
+  },
+  button: {
+    fontSize: 10
+  },
+  buttonText: {
+    fontSize: 13
+  },
+  error: {
+    color: "red",
+    fontSize: 20,
+    marginTop: 20,
+    alignContent: "center",
+    flex: 1
+  }
+});
