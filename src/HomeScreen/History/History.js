e0c97cf3159bcb2ed4a223e66599562be0442686
Paul Bond <paulandrewbond@outlook.com>
2020-02-24 11:37:09 +0000

    add "screen" to name

--- src/HomeScreen/History/History.js
+++ src/HomeScreen/History/History.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
 
-export default class History extends Component {
+export default class HistoryScreen extends Component {
   render() {
     return (
-      <View>
-        <Text> History </Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>History!</Text>
       </View>
     );
   }
