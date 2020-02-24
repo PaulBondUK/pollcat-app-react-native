70672c5bd293ac7ee720e603a86cbde7f126c70d
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-02-24 15:48:27 +0000

    Changes to today's poll

--- src/HomeScreen/TodaysPoll/TodaysPoll.js
+++ src/HomeScreen/TodaysPoll/TodaysPoll.js
@@ -1,12 +1,63 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
+import React, { Component, Fragment } from "react";
+import { TextInput, View, StyleSheet, AsyncStorage } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  FooterTab,
+  Icon,
+  Text,
+  Form,
+  Item,
+  Input,
+  Label,
+  Left,
+  Right,
+  Body,
+  Title,
+  Button,
+  H1,
+  Spinner,
+  Card,
+  CardItem
+} from "native-base";
+import { questions, answers } from "../../../spec/TestData";
 
 export default class TodaysPollScreen extends Component {
+  state = {
+    questions: null,
+    isLoading: true
+  };
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Today's Poll!</Text>
-      </View>
+      <Fragment>
+        <Header />
+
+        <Content
+          contentContainerStyle={{
+            flex: 1,
+            backgroundColor: "white"
+          }}
+          style={{ flex: 1 }}
+        >
+          <Card>
+            <CardItem>
+              <Body>
+                <H1>
+                  {this.state.questions && this.state.questions[0].question}
+                </H1>
+              </Body>
+            </CardItem>
+          </Card>
+          <Text>Hello!!!!!</Text>
+        </Content>
+      </Fragment>
     );
+  }
+
+  componentDidMount() {
+    this.setState({ questions, isLoading: false });
   }
 }
