5ae67f20c57c38e7ef6ea57315bfbdb01c18d887
Paul Bond <paulandrewbond@outlook.com>
2020-02-24 22:24:35 +0000

    add history page

--- src/HomeScreen/History/History.js
+++ src/HomeScreen/History/History.js
@@ -1,12 +1,72 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
+import React, { PureComponent, Fragment } from "react";
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  AsyncStorage,
+  Image,
+  Dimensions
+} from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  FooterTab,
+  Icon,
+  Text,
+  Form,
+  Item,
+  Input,
+  Label,
+  Left,
+  Right,
+  Body,
+  Title,
+  Button,
+  H1,
+  Spinner,
+  Card,
+  CardItem
+} from "native-base";
+import PollCard from "../TodaysPoll/PollCard";
+import { monthName } from "../../Utils/DateFormatting";
+import { questions } from "../../../spec/TestData";
 
-export default class HistoryScreen extends Component {
+export default class HistoryScreen extends PureComponent {
+  state = {
+    questionData: null,
+    isLoading: true
+  };
+  const;
   render() {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>History!</Text>
-      </View>
-    );
+    const { isLoading, questionData } = this.state;
+    if (isLoading) {
+      return (
+        <Container>
+          <Content>
+            <Spinner />
+          </Content>
+        </Container>
+      );
+    } else if (questionData) {
+      return (
+        <Container>
+          <Content>
+            {questionData.map((question, index) => {
+              return <PollCard key={index} questionData={question} />;
+            })}
+          </Content>
+        </Container>
+      );
+    }
+  }
+
+  componentDidMount() {
+    const questionData = questions.filter(question => {
+      return question.questionStatus === "past";
+    });
+
+    this.setState({ questionData, isLoading: false });
   }
 }
