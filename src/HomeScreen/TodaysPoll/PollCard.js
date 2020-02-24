88754afa3f9f9ad012ea3b95255c9ad6928128d5
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-02-24 15:48:08 +0000

    Added PollCard

---
+++ src/HomeScreen/TodaysPoll/PollCard.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { TextInput, View, StyleSheet, AsyncStorage } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  FooterTab,
+  Icon,
+  Text,
+  Form,
+  Item,
+  Input,
+  Label,
+  Left,
+  Right,
+  Body,
+  Title,
+  Button,
+  H1,
+  Spinner,
+  Card
+} from "native-base";
+import { questions, answers } from "../../../spec/TestData";
+
+const PollCard = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default PollCard;
