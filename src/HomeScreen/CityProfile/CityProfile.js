482a9d11bab4e0e16ededc1f448e4ceee855c525
Paul Bond <paulandrewbond@outlook.com>
2020-02-24 11:37:01 +0000

    add 'screen' to name

--- src/HomeScreen/CityProfile/CityProfile.js
+++ src/HomeScreen/CityProfile/CityProfile.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
 
-export default class CityProfile extends Component {
+export default class CityProfileScreen extends Component {
   render() {
     return (
-      <View>
-        <Text> CityProfile </Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text> City Profile! </Text>
       </View>
     );
   }
