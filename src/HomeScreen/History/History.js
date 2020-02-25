55cd5da94636cd7b6ebb08941b80b6f5c24fd31a
Paul Bond <paulandrewbond@outlook.com>
2020-02-25 22:13:23 +0000

    add card styling

--- src/HomeScreen/History/History.js
+++ src/HomeScreen/History/History.js
@@ -5,7 +5,8 @@ import {
   StyleSheet,
   AsyncStorage,
   Image,
-  Dimensions
+  Dimensions,
+  SafeAreaView
 } from "react-native";
 import {
   Container,
@@ -32,6 +33,7 @@ import {
 import PollCard from "../TodaysPoll/PollCard";
 import { monthName } from "../../Utils/DateFormatting";
 import { questions } from "../../../spec/TestData";
+import * as Api from "../../../Api";
 
 export default class HistoryScreen extends PureComponent {
   state = {
@@ -40,19 +42,39 @@ export default class HistoryScreen extends PureComponent {
   };
   const;
   render() {
+    const now = new Date();
     const { isLoading, questionData } = this.state;
+    console.log(isLoading, questionData);
     if (isLoading) {
       return (
         <Container>
-          <Content>
-            <Spinner />
+          <Content
+            contentContainerStyle={{
+              flex: 1,
+              justifyContent: "center",
+              paddingTop: 50
+            }}
+          >
+            <Spinner color={"tomato"} />
           </Content>
         </Container>
       );
     } else if (questionData) {
       return (
         <Container>
-          <Content>
+          <Header noShadow style={{ height: 80 }}>
+            <View
+              style={{
+                paddingLeft: 10,
+                paddingBottom: 10,
+                flex: 1,
+                justifyContent: "flex-end"
+              }}
+            >
+              <Text style={{ fontSize: 32, fontWeight: "bold" }}>History</Text>
+            </View>
+          </Header>
+          <Content style={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}>
             {questionData.map((question, index) => {
               return <PollCard key={index} questionData={question} />;
             })}
@@ -62,11 +84,39 @@ export default class HistoryScreen extends PureComponent {
     }
   }
 
-  componentDidMount() {
-    const questionData = questions.filter(question => {
-      return question.questionStatus === "past";
-    });
+  //   componentDidMount() {
+  //     const questionData = questions.filter(question => {
+  //       return question.questionStatus === "past";
+  //     });
 
-    this.setState({ questionData, isLoading: false });
+  //     this.setState({ questionData, isLoading: false });
+  //   }
+  // }
+
+  componentDidMount() {
+    Api.getQuestions()
+      .then(({ questions }) => {
+        return (questionData = questions.filter(question => {
+          return question.questionStatus === "past";
+        }));
+      })
+      .then(returnedQuestionData => {
+        console.log(returnedQuestionData);
+
+        const questionData = returnedQuestionData.map(question => {
+          const startTime = Date.parse(question.startTime);
+          const endTime = startTime + 86400;
+          return { ...question, startTime, endTime };
+        });
+        console.log(questionData);
+
+        this.setState({
+          questionData,
+          isLoading: false
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
