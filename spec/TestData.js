e4d06bcda123d017e8f9a5d7257e3a532068e615
Paul Bond <paulandrewbond@outlook.com>
2020-02-25 09:09:58 +0000

    added exclamations(!)

--- spec/TestData.js
+++ spec/TestData.js
@@ -5,15 +5,15 @@ exports.questions = [
     questionStatus: "current",
     img: "https://i.postimg.cc/7ZPTGpGR/marmite.jpg",
     answerArray: [
-      { answer: "Love it", img: "answer photo" },
-      { answer: "Hate it", img: "answer photo" }
+      { answer: "Love it!", img: "answer photo" },
+      { answer: "Hate it!", img: "answer photo" }
     ]
   },
   {
     question: "Best Burger in Manchester?",
     startTime: 1582660800,
     questionStatus: "past",
-    img: "question photo",
+    img: "https://i.postimg.cc/vHYgvBry/burger.jpg",
     answerArray: [
       { answer: "Solita", img: "answer photo", votes: 187 },
       { answer: "Almost Famous", img: "answer photo", votes: 256 },
@@ -27,28 +27,28 @@ exports.questions = [
     question: "Custard Creams or Bourbons?",
     startTime: 1582747200,
     questionStatus: "past",
-    img: "question photo",
+    img: "https://i.postimg.cc/76rbJYHp/custard-creams-or-bourbons.jpg",
     answerArray: [
-      { answer: "Custard Creams", img: "answer photo", votes: 543 },
-      { answer: "Bourbons", img: "answer photo", votes: 679 }
+      { answer: "Custard Creams!", img: "answer photo", votes: 543 },
+      { answer: "Bourbons!", img: "answer photo", votes: 679 }
     ]
   },
   {
-    question: "Liam or Noel?",
+    question: "Noel or Liam?",
     startTime: 1582574400,
     questionStatus: "past",
-    img: "question photo",
+    img: "https://i.postimg.cc/PqY5HsWK/noel-or-liam.jpg",
     answerArray: [
-      { answer: "Liam", img: "answer photo", votes: 830 },
-      { answer: "Noel", img: "answer photo", votes: 869 },
-      { answer: "Neither", img: "answer photo", votes: 612 }
+      { answer: "Liam!", img: "answer photo", votes: 830 },
+      { answer: "Noel!", img: "answer photo", votes: 869 },
+      { answer: "Neither!", img: "answer photo", votes: 612 }
     ]
   },
   {
     question: "Best Pizza in Manchester?",
     startTime: 1582488000,
     questionStatus: "past",
-    img: "question photo",
+    img: "https://i.postimg.cc/gjSYsYpc/pizza.jpg",
     answerArray: [
       { answer: "Rudy's", img: "answer photo", votes: 498 },
       { answer: "Double Zero", img: "answer photo", votes: 461 },
@@ -58,36 +58,36 @@ exports.questions = [
     ]
   },
   {
-    question: "City or United?",
+    question: "United or City?",
     startTime: 1582401600,
     questionStatus: "past",
-    img: "question photo",
+    img: "https://i.postimg.cc/L654XZkJ/united-or-city.jpg",
     answerArray: [
-      { answer: "City", img: "answer photo", votes: 919 },
-      { answer: "United", img: "answer photo", votes: 743 }
+      { answer: "City!", img: "answer photo", votes: 919 },
+      { answer: "United!", img: "answer photo", votes: 743 }
     ]
   },
   {
     question: "Black Pudding...",
     startTime: 1582315200,
     questionStatus: "past",
-    img: "question photo",
+    img: "https://i.postimg.cc/SRqX3QHW/black-pudding.jpg",
     answerArray: [
-      { answer: "On my plate", img: "answer photo", votes: 509 },
-      { answer: "In the bin", img: "answer photo", votes: 1134 }
+      { answer: "On my plate!", img: "answer photo", votes: 509 },
+      { answer: "In the bin!", img: "answer photo", votes: 1134 }
     ]
   },
   {
     question: "I like my eggs...",
     startTime: 1582228800,
     questionStatus: "past",
-    img: "question photo",
+    img: "https://i.postimg.cc/vTQBXq6h/eggs.jpg",
     answerArray: [
       { answer: "Fried", img: "answer photo", votes: 383 },
       { answer: "Boiled", img: "answer photo", votes: 222 },
       { answer: "Poached", img: "answer photo", votes: 198 },
       { answer: "Scrambled", img: "answer photo", votes: 345 },
-      { answer: "In the bin", img: "answer photo", votes: 170 },
+      { answer: "In the bin!", img: "answer photo", votes: 170 },
       { answer: "No thanks, I'm vegan", img: "answer photo", votes: 12 }
     ]
   }
