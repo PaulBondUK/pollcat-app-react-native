28f8f67e27b11ce4daa8f31687faa57da39518ba
Paul Bond <paulandrewbond@outlook.com>
2020-02-25 15:54:07 +0000

    moved cards to CDM

--- src/HomeScreen/TodaysPoll/TodaysPoll.js
+++ src/HomeScreen/TodaysPoll/TodaysPoll.js
@@ -37,43 +37,49 @@ import ConfettiCannon from "react-native-confetti-cannon";
 import CountDown from "react-native-countdown-component";
 import CardList from "react-native-card-animated-modal";
 import { PollCardToday } from "./PollCardToday";
+import CARDS from "./CardGeneratorToday";
+import * as Api from "../../../Api";
 
 export default class TodaysPollScreen extends PureComponent {
   state = {
     questionData: null,
     isLoading: true,
-    endTime: null
+    endTime: null,
+    CARDS: null
   };
 
   render() {
     const now = new Date();
-    const CARDS = [
-      {
-        image: {
-          uri: "https://i.postimg.cc/7ZPTGpGR/marmite.jpg"
-        },
-        height: 500,
-        renderItem: ({ item, index }) => (
-          // <View>
-          //   <Text
-          //     style={{ color: "rgba(0, 0, 0, 0.7)", fontSize: 30, paddingLeft: 10 }}
-          //   >
-          //     Marmite...
-          //   </Text>
-          // </View>
-          <PollCardToday endTime={this.state.endTime} />
-        ),
-        renderDetails: ({ item, index }) => (
-          /* You can also provide custom content per item */
-          <View style={{ paddingVertical: 30, paddingHorizontal: 16 }}>
-            <Text style={{ color: "rgba(0, 0, 0, 0.7)", fontSize: 18 }}>
-              Test text
-            </Text>
-          </View>
-        )
-      }
-    ];
-    const { isLoading, questionData, endTime } = this.state;
+    // const CARDS = [
+    //   {
+    //     image: {
+    //       uri: "https://i.postimg.cc/7ZPTGpGR/marmite.jpg"
+    //     },
+    //     height: 500,
+    //     renderItem: ({ item, index }) => (
+    //       <PollCardToday endTime={this.state.endTime} />
+    //     ),
+    //     renderDetails: ({ item, index }) => (
+    //       <View
+    //         style={{
+    //           paddingVertical: 30,
+    //           paddingHorizontal: 16
+    //         }}
+    //       >
+    //         <Text
+    //           style={{
+    //             color: "rgba(0, 0, 0, 0.7)",
+    //             fontSize: 18
+    //           }}
+    //         >
+    //           Test text
+    //         </Text>
+    //       </View>
+    //     )
+    //   }
+    // ];
+
+    const { isLoading, questionData, endTime, CARDS } = this.state;
     const today = new Date();
 
     if (isLoading) {
@@ -86,38 +92,32 @@ export default class TodaysPollScreen extends PureComponent {
       );
     } else {
       return (
-        <>
-          <CardList
-            listProps={{
-              ListHeaderComponent: () => (
-                <View style={{ padding: 16, paddingBottom: 0 }}>
-                  <Text
-                    style={{
-                      fontSize: 13,
-                      color: "rgba(0, 0, 0, 0.5)"
-                    }}
-                  >
-                    {now.toDateString()}
-                  </Text>
-                  <Text style={{ fontSize: 32, fontWeight: "bold" }}>
-                    Today's Poll
-                  </Text>
-                </View>
-              )
-            }}
-            data={CARDS}
-            renderItem={({ item, index }) => {
-              /* Render card per item */
-              return item.renderItem({ item, index });
-
-              /* Default card when not specified */
-            }}
-            renderDetails={({ item, index }) => {
-              /* You can also provide custom content per item */
-              return item.renderDetails({ item, index });
-            }}
-          />
-        </>
+        <CardList
+          listProps={{
+            ListHeaderComponent: () => (
+              <View style={{ padding: 16, paddingBottom: 0 }}>
+                <Text
+                  style={{
+                    fontSize: 15,
+                    color: "rgba(0, 0, 0, 0.5)"
+                  }}
+                >
+                  {now.toDateString()}
+                </Text>
+                <Text style={{ fontSize: 32, fontWeight: "bold" }}>
+                  Today's Poll
+                </Text>
+              </View>
+            )
+          }}
+          data={CARDS}
+          renderItem={({ item, index }) => {
+            return item.renderItem({ item, index });
+          }}
+          renderDetails={({ item, index }) => {
+            return item.renderDetails({ item, index });
+          }}
+        />
         // <Container>
         //   {/* <ConfettiCannon
         //     count={100}
@@ -162,14 +162,67 @@ export default class TodaysPollScreen extends PureComponent {
   }
 
   componentDidMount() {
-    const questionData = questions.find(question => {
-      return question.questionStatus === "current";
-    });
+    Api.getQuestions()
+      .then(({ questions }) => {
+        console.log(questions);
+        return (questionData = questions.find(question => {
+          return question.questionStatus === "current";
+        }));
+      })
+      .then(questionData => {
+        const startTime = Date.parse(questionData.startTime);
+        const { img, answerArray } = questionData;
+        // const parsedAnswerArray = JSON.parse(answerArray);
+        // console.log(parsedAnswerArray);
+        const endTime = (startTime + 86400) * 1000;
+        const CARDS = [
+          {
+            image: {
+              uri: img
+            },
+            height: 500,
+            renderItem: ({ item, index }) => (
+              <PollCardToday endTime={endTime} />
+            ),
+            renderDetails: ({ item, index }) => (
+              <View
+                style={{
+                  paddingVertical: 30,
+                  paddingHorizontal: 16
+                }}
+              >
+                <Text
+                  style={{
+                    color: "rgba(0, 0, 0, 0.7)",
+                    fontSize: 18
+                  }}
+                >
+                  Test text
+                </Text>
+              </View>
+            )
+          }
+        ];
+        this.setState({
+          questionData,
+          isLoading: false,
+          endTime,
+          CARDS
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
 
-    const { startTime } = questionData;
-    const endTime = (startTime + 86400) * 1000;
+    // const { startTime } = questionData;
+    // const endTime = (startTime + 86400) * 1000;
 
-    this.setState({ questionData, isLoading: false, endTime });
+    // this.setState({
+    //   questionData,
+    //   isLoading: false,
+    //   endTime,
+    //   CARDS
+    // });
   }
 }
 
