678f2d7ff26d45725c09cf51d876d96ddb901fa1
Paul Bond <paulandrewbond@outlook.com>
2020-02-26 07:54:06 +0000

    add single poll from params

--- src/HomeScreen/History/SinglePollHistory.js
+++ src/HomeScreen/History/SinglePollHistory.js
@@ -1,18 +1,66 @@
-import React from "react";
-import { View, Text, Image, Dimensions } from "react-native";
+import React, { PureComponent } from "react";
+import { View, Image, Dimensions } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  FooterTab,
+  Icon,
+  Text,
+  Form,
+  Item,
+  Input,
+  Label,
+  Left,
+  Right,
+  Body,
+  Title,
+  Button,
+  H1,
+  Spinner,
+  Card,
+  CardItem
+} from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+import ConfettiCannon from "react-native-confetti-cannon";
+import { render } from "react-dom";
 
-const height = (Dimensions.get("window").width / 800) * 500;
+// const height = (Dimensions.get("window").width / 800) * 500;
+const width = Dimensions.get("window").width;
+const height = (width / 800) * 500;
 
-const SinglePollHistory = ({ route, navigation }) => {
-  const { params } = route;
-  const { img, question } = params;
-  console.log("QUESTIONS!!!!", params);
-  return (
-    <View>
-      <Text>{question}</Text>
-      <Image source={{ uri: img }} style={{ height: height, flex: 1 }} />
-    </View>
-  );
-};
-
-export default SinglePollHistory;
+export default class SinglePollHistory extends PureComponent {
+  state = {
+    showConfetti: false
+  };
+  render() {
+    const { params } = this.props.route;
+    const { img, question } = params;
+    return (
+      <Container>
+        <Content>
+          {this.state.showConfetti && (
+            <ConfettiCannon
+              count={100}
+              origin={{ x: 0, y: 0 }}
+              fadeOut={true}
+            />
+          )}
+          <Image source={{ uri: img }} style={{ height: height, flex: 1 }} />
+          <Text style={{ fontSize: 25, fontWeight: "bold" }}>{question}</Text>
+          <Button
+            style={{ backgroundColor: "tomato", height: 60, margin: 15 }}
+            onPress={() => {
+              this.setState({ showConfetti: true });
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+              Reveal the Winner...
+            </Text>
+          </Button>
+        </Content>
+      </Container>
+    );
+  }
+}
