530fafc28ce93d39163daad11c2f1ded31a015bf
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-02-26 16:28:06 +0000

    ComponentDidMount and added to State

--- src/HomeScreen/CityProfile/CityProfile.js
+++ src/HomeScreen/CityProfile/CityProfile.js
@@ -1,12 +1,85 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import {
+  SafeAreaView,
+  KeyboardAvoidingView,
+  TextInput,
+  View,
+  StyleSheet,
+  AsyncStorage
+} from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Text,
+  Spinner,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button
+} from "native-base";
+import firebase from "../../Auth/Firebase";
+import * as Api from "../../../Api";
 
 export default class CityProfileScreen extends Component {
+  state = {
+    questionData: "",
+    isLoading: true,
+    endTime: null,
+    parsedAnswerArray: null,
+    votedAnswer: null
+  };
+
   render() {
+    const {
+      questionData,
+      isLoading,
+      endTime,
+      parsedAnswerArray,
+      votedAnswer
+    } = this.state;
+
+    // if (isLoading) {
+    //   return (
+    //     <Container>
+    //       <Content
+    //         contentContainerStyle={{
+    //           flex: 1,
+    //           justifyContent: "center",
+    //           paddingTop: 50
+    //         }}
+    //       >
+    //         <Spinner color={"tomato"} />
+    //       </Content>
+    //     </Container>
+    //   );
+    // } else {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
         <Text> City Profile! </Text>
       </View>
     );
+  }
+  //}
+  componentDidMount() {
+    //changed question status to "past"
+    Api.getQuestions({ questionStatus: "past" }).then(({ questions }) => {
+      const questionData = questions[0];
+      console.log(questionData, "QUESTION DATA");
+      const startTime = Date.parse(questionData.startTime);
+      console.log(startTime, "START TIME");
+      const { img, answerArray } = questionData;
+      const parsedAnswerArray = answerArray.map(answer => {
+        console.log(JSON.parse(answer), "PARSED ANSWER");
+        return JSON.parse(answer);
+      });
+    });
   }
 }
