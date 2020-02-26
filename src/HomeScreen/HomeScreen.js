fc8b20079b1d8802c235cf74530c9954427f4984
Paul Bond <paulandrewbond@outlook.com>
2020-02-26 12:12:35 +0000

    cleaned up code

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -35,13 +35,6 @@ import TodaysPollScreen from "./TodaysPoll/TodaysPoll";
 import AccountRouter from "./Account/AccountRouter";
 import TabBarIcon from "../HomeScreen/TabBarIcon";
 
-// const Tab = createBottomTabNavigator({
-//   "Today's Poll": TodaysPollScreen,
-//   History: HistoryScreen,
-//   "City Profile": CityProfileScreen,
-//   Account: AccountScreen
-// });
-
 const Tab = createBottomTabNavigator();
 
 export default class HomeScreen extends Component {
@@ -49,21 +42,18 @@ export default class HomeScreen extends Component {
     user: null,
     location: null,
     townName: null,
-    CountyName: null,
+    countyName: null,
     error: null,
     isLoading: true,
     activePage: "Today's Poll"
   };
 
   render() {
-    // if (this.state.user && !this.state.user.displayName) {
-    //   this.props.navigation.navigate("CreateDisplayName");
-    // }
     const {
       user,
       location,
       townName,
-      CountyName,
+      countyName,
       activePage,
       isLoading
     } = this.state;
@@ -97,6 +87,7 @@ export default class HomeScreen extends Component {
                   <TabBarIcon name="today" focused={focused} />
                 )
               }}
+              initialParams={{ user, townName, countyName }}
             />
             <Tab.Screen
               name="History"
@@ -130,116 +121,9 @@ export default class HomeScreen extends Component {
       </Fragment>
     );
   }
-  // <Container>
-  // <Header>
-  //   <Left />
-  //   <Body>
-  //     <Title>{activePage}</Title>
-  //     <Right />
-  //   </Body>
-  //   <Right />
-  // </Header>
-  //   <Content
-  //     style={{ flex: 1 }}
-  //     contentContainerStyle={{
-  //       flex: 1,
-  //       justifyContent: "center",
-  //       alignItems: "center"
-  //     }}
-  //   >
-  //     <H1 style={styles.header1}>
-  //       {townName && `Viewing Polls in ${townName}`}
-  //     </H1>
-  //   </Content>
-  //   <Footer>
-  //     <FooterTab>
-  //       <Button
-  //         style={styles.button}
-  //         vertical
-  //         onPress={() => this.setState({ activePage: "Today's Poll" })}
-  //         active={this.state.activePage === "Today's Poll"}
-  //       >
-  //         <Icon type="MaterialCommunityIcons" name="calendar-today" />
-  //         <Text style={styles.buttonText}>Today's Poll</Text>
-  //       </Button>
-  //       <Button
-  //         style={styles.button}
-  //         vertical
-  //         onPress={() => this.setState({ activePage: "History" })}
-  //         active={this.state.activePage === "History"}
-  //       >
-  //         <Icon type="MaterialCommunityIcons" name="history" />
-  //         <Text style={styles.buttonText}>History</Text>
-  //       </Button>
-  //       <Button
-  //         style={styles.button}
-  //         vertical
-  //         onPress={() => this.setState({ activePage: "City Profile" })}
-  //         active={this.state.activePage === "City Profile"}
-  //       >
-  //         <Icon type="MaterialCommunityIcons" name="city" />
-  //         <Text style={styles.buttonText}>City Profile</Text>
-  //       </Button>
-  //       <Button
-  //         style={styles.button}
-  //         vertical
-  //         onPress={() => this.setState({ activePage: "Account" })}
-  //         active={this.state.activePage === "Account"}
-  //       >
-  //         <Icon type="MaterialCommunityIcons" name="account" />
-  //         <Text style={styles.buttonText}>Account</Text>
-  //       </Button>
-  //     </FooterTab>
-  //   </Footer>
-  // </Container>
-
-  // firebaseUserCheck = () => {
-  //   const user = firebase.auth().currentUser;
-
-  //   if (user) {
-  //     console.log(user, "user is logged in");
-  //   } else {
-  //     console.log("user is not logged in");
-  //     this.props.navigation.navigate("LoginOrCreate");
-  //   }
-  // };
-
-  // async getLocationAsync() {
-  //   let { status } = await Permissions.askAsync(Permissions.LOCATION);
-  //   if (status !== "granted") {
-  //     this.setState({
-  //       error:
-  //         "Please enable Location Services in your settings to join in on Pollcat"
-  //     });
-  //   }
-  //   let location = await Location.getCurrentPositionAsync({});
-  //   this.setState({ location });
-  //   return location;
-  // }
-
-  // getPlaceName = location => {
-  //   return axios
-  //     .get(
-  //       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.coords.latitude},${location.coords.longitude}&result_type=administrative_area_level_2&key=${GoogleMapsAuth}`
-  //     )
-  //     .then(({ data }) => {
-  //       this.setState({
-  //         placeName: data.results[0].address_components[0].long_name
-  //       });
-  //       return data.results;
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //       this.setState({ error });
-  //     });
-  // };
 
   async componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
-      // if (user && !user.displayName) {
-      //   this.props.navigation.navigate("CreateDisplayName");
-      // } else
-
       if (user) {
         this.setState({ user });
       } else {
