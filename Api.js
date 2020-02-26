499c008b34d59c37b97d81ecb0c983902e616394
Paul Bond <paulandrewbond@outlook.com>
2020-02-26 12:12:43 +0000

    add post answer

--- Api.js
+++ Api.js
@@ -10,3 +10,33 @@ exports.getQuestions = args => {
       return data;
     });
 };
+
+exports.postAnswer = ({
+  question_id,
+  userUid,
+  answerIndex,
+  townName,
+  countyName
+}) => {
+  return axios
+    .post(`https://pollcat-backend.herokuapp.com/api/answers`, {
+      question_id,
+      userUid,
+      answerIndex,
+      townName,
+      countyName
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.checkIfUserHasVoted = (question_id, userUid) => {
+  return axios
+    .get(
+      `https://pollcat-backend.herokuapp.com/api/questions/${question_id}/answers/${userUid}`
+    )
+    .then(({ data }) => {
+      console.log(data);
+    });
+};
