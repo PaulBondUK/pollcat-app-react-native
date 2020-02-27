67f924b8cbe92bd1f0b2728a500f1b1fa4883327
Paul Bond <paulandrewbond@outlook.com>
2020-02-27 19:31:50 +0000

    add loading screen

--- src/HomeScreen/History/SinglePollHistory.js
+++ src/HomeScreen/History/SinglePollHistory.js
@@ -32,34 +32,121 @@ const height = (width / 800) * 500;
 
 export default class SinglePollHistory extends PureComponent {
   state = {
-    showConfetti: false,
-    revealWinner: false
+    showResults: false,
+    revealWinner: false,
+    isLoading: true,
+    answerArray: null
   };
   render() {
     const { params } = this.props.route;
     const { img, question } = params;
-    return (
-      <>
-        <View style={{ position: "absolute" }}>
-          <Confetti active={true} elementCount={100} />
-        </View>
+    const { answerArray, isLoading, revealWinner } = this.state;
+    if (isLoading) {
+      return (
+        <Container>
+          <Content
+            contentContainerStyle={{
+              flex: 1,
+              justifyContent: "center",
+              paddingTop: 50
+            }}
+          >
+            <Spinner color={"tomato"} />
+          </Content>
+        </Container>
+      );
+    } else {
+      return (
         <Container>
           <Content>
             <Image source={{ uri: img }} style={{ height: height, flex: 1 }} />
-            <Text style={{ fontSize: 25, fontWeight: "bold" }}>{question}</Text>
-            <Button
-              style={{ backgroundColor: "tomato", height: 60, margin: 15 }}
-              onPress={() => {
-                this.setState({ showConfetti: true });
+            <Text
+              style={{
+                fontSize: 25,
+                fontWeight: "bold",
+                padding: 20
               }}
             >
-              <Text style={{ fontSize: 20, fontWeight: "bold" }}>
-                Reveal the Winner...
-              </Text>
-            </Button>
+              {question}
+            </Text>
+            {!revealWinner && (
+              <Button
+                style={{
+                  backgroundColor: "tomato",
+                  height: 70,
+                  marginLeft: 15,
+                  marginRight: 15
+                }}
+                onPress={() => {
+                  this.setState({ revealWinner: true });
+                }}
+              >
+                <Text style={{ fontSize: 30, fontWeight: "bold" }}>
+                  Reveal the Winner...
+                </Text>
+              </Button>
+            )}
+            {revealWinner &&
+              answerArray.map((answer, index) => {
+                return (
+                  <Card
+                    key={index}
+                    style={{
+                      marginLeft: 15,
+                      marginRight: 15
+                    }}
+                  >
+                    <CardItem
+                      style={
+                        index === 0
+                          ? {
+                              flex: 1,
+                              justifyContent: "space-between",
+                              backgroundColor: "#20C5B6"
+                            }
+                          : {
+                              flex: 1,
+                              justifyContent: "space-between",
+                              backgroundColor: "tomato"
+                            }
+                      }
+                    >
+                      <Text
+                        style={{
+                          fontWeight: "bold",
+                          fontSize: 20,
+                          color: "white"
+                        }}
+                      >
+                        {answer.answer}
+                      </Text>
+
+                      <Text
+                        style={{
+                          fontSize: 20,
+                          color: "white"
+                        }}
+                      >{`${answer.votes} votes`}</Text>
+                    </CardItem>
+                  </Card>
+                );
+              })}
           </Content>
         </Container>
-      </>
-    );
+      );
+    }
+  }
+
+  componentDidMount() {
+    const { params } = this.props.route;
+    const { img, question, answerArray } = params;
+    const parsedAnswerArray = answerArray.map(answer => {
+      return JSON.parse(answer);
+    });
+    parsedAnswerArray.sort((a, b) => b.votes - a.votes);
+    this.setState({
+      isLoading: false,
+      answerArray: parsedAnswerArray
+    });
   }
 }
